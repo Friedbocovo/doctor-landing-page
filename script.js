092a5118f24a4b8ef96d249cf0a5ebe3a43d764e
Friedbocovo <friedbocovo03@gmail.com>
2024-08-08 10:48:32 +0100

    new modification

---
+++ script.js
@@ -0,0 +1,10 @@
+const iconMenu=document.querySelector(".menu")
+const menu=document.querySelector("  .section1__header__nav__menu")
+const menuLinkActive=document.querySelector(".section1__header__nav__menu__links")
+
+iconMenu.addEventListener("click",()=>{
+
+
+  menuLinkActive.classList.add("timeout")
+  menuLinkActive.classList.toggle("active-menu")
+})
