21b7bb0fc99601f8d150ac0948a483fabe96e2cf
Friedbocovo <friedbocovo03@gmail.com>
2024-08-30 01:58:09 +0100

    refactor:code removal

--- script.js
+++
@@ -1,8 +0,0 @@
-const iconMenu=document.querySelector(".menu")
-const menu=document.querySelector("  .section1__header__nav__menu")
-const menuLinkActive=document.querySelector(".section1__header__nav__menu")
-
-iconMenu.addEventListener("click",()=>{
-  menuLinkActive.classList.add("timeout")
-  menuLinkActive.classList.toggle("active-menu")
-})
